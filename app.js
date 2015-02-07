52c4d09f334347273870943b7a1afbe988c7e522
CodyAlanSanders <cody.alan.sanders@gmail.com>
2015-02-07 14:08:13 -0500

    Task 2 Complete
    Added properties to the page for a product.
    
    Added Angular and Bootstrap.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var app = angular.module('gemStore', []);
+(function() { //wrapping it in a closure
+	var app = angular.module('gemStore', []);
+	
+	//Name the controller and then anonymous function
+	app controller('StoreController', function(){
+		this.product = gem;
+	});
+	
+	//gem variable
+	var gem = {
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: '...'
+	}
+	
+})();  //end closure
